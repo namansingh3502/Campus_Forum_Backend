0f345c5c13660910e13118e39358ff713b5c03b8
namansigh3502 <namansingh0305@gmail.com>
2022-01-11 23:07:00 +0530

    workimng in post create UI

--- frontend/src/components/Post_Column/post/Create_Post/postCreateModal.js
+++ frontend/src/components/Post_Column/post/Create_Post/postCreateModal.js
@@ -12,20 +12,35 @@ export default function PostCreateModal(){
       id={'postCreateModal'}
     >
       <div
-        className={"h-2/5 w-1/3 relative top-20 mx-auto border shadow-lg rounded-md  body"}
+        className={"h-auto w-3/12 relative top-20 mx-auto border shadow-lg rounded-md  body"}
         style={{backgroundColor:'#011627'}}
       >
         <div className={"bg-gray-400 rounded-lg bg-opacity-10 backdrop-filter backdrop-blur-xl h-full w-full"}>
           <div className={"divide-y divide-gray-700"}>
-            <div className="border-b px-4 py-2 flex justify-between items-center font-bold">
-              <h3 className="text-2xl">Create Post</h3>
-              <button><AiOutlineClose className="close-modal "/></button>
+            <div className="border-b pl-6 pr-4 py-4 flex justify-between items-center">
+              <h3 className="text-2xl font-medium">Create Post</h3>
+              <button><AiOutlineClose className="close-modal"/></button>
             </div>
-
-            <div className="flex p-5">
-              <img src={userImage} className="rounded-full" style={{height:50, width:50}} alt={"user"}/>
-              <div className="ml-4 text-bold">
-                <h1 className="text-md font-bold" id={user_id}>{username}</h1>
+            <div className={"p-4"}>
+              <div className="flex p-2">
+                <img
+                  src={userImage} className="rounded-full"
+                  style={{height:50, width:50}}
+                  alt={"user"}
+                />
+                <div className="ml-4 text-bold">
+                  <h1 className="text-md font-bold" id={user_id}>{username}</h1>
+                </div>
+              </div>
+              <div className={"px-2 py-4 text-black"}>
+                <textarea
+                  className="resize-none w-full h-32 p-2 bg-transparent focus:outline-0 text-white text-xl"
+                  placeholder={"What do you want to talk about?"}
+                  style={{outline:"none"}}
+                />
+              </div>
+              <div>
+                <button className="w-full mx-2 bg-blue-400 mx-auto text-lg rounded-md p-2">Post</button>
               </div>
             </div>
           </div>
